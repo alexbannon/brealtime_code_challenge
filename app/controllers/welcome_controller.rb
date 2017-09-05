a32e9d408df38bea105a28f82c6321530039a6b3
Alex Bannon <alexbannon@Kevins-MacBook-Air-2.local>
2017-09-04 21:22:00 -0400

    more routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
 
   def index
+    render :status => 200, :json => {}
+  end
+
+  def ok
+    render :json => {hello: "world"}
+    head :no_content
   end
 
 end
